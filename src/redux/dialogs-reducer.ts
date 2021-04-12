c47a0846e9d5a498ecebcaf3e0bde00a9ad7221e
nikolay <fadeev1999nikolay@gmail.com>
2021-04-12 22:54:14 +0300

    fix bug with immutability of reducers

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -1,57 +1,60 @@
-import {ActionType} from "./redux-store";
+import { ActionType } from "./redux-store";
 
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
 export type DialogType = {
-    id: number,
-    name: string
+  id: number,
+  name: string
 }
 
 export type MessageType = {
-    id: number,
-    message: string
+  id: number,
+  message: string
 }
 
 export type DialogsPageStateType = {
-    dialogs: DialogType[],
-    messages: MessageType[],
-    newMessageBody: string
+  dialogs: DialogType[],
+  messages: MessageType[],
+  newMessageBody: string
 };
 
 const initialState: DialogsPageStateType = {
-    dialogs: [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrei'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Viktor'},
-        {id: 5, name: 'Valera'},
-    ],
-    messages: [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your it?'},
-        {id: 3, message: 'yo'},
-    ],
-    newMessageBody: ''
+  dialogs: [
+    { id: 1, name: 'Dimych' },
+    { id: 2, name: 'Andrei' },
+    { id: 3, name: 'Sveta' },
+    { id: 4, name: 'Viktor' },
+    { id: 5, name: 'Valera' },
+  ],
+  messages: [
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'How is your it?' },
+    { id: 3, message: 'yo' },
+  ],
+  newMessageBody: ''
 }
 
-export const dialogsReducer = (state:DialogsPageStateType = initialState, action: ActionType) => {
-    switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
-        case SEND_MESSAGE:
-            let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messages.push({id: 4, message: body});
-            return state;
-        default:
-            return state;
-    }
+export const dialogsReducer = (state: DialogsPageStateType = initialState, action: ActionType) => {
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {
+        ...state,
+        newMessageBody: action.body
+      }
+    case SEND_MESSAGE:
+      return {
+        ...state,
+        newMessageBody: '',
+        messages: [...state.messages, { id: 4, message: state.newMessageBody }]
+      }
+    default:
+      return state;
+  }
 }
 
 export type updateNewMessageBodyActionType = { type: typeof UPDATE_NEW_MESSAGE_BODY, body: string };
 export type sendMessageActionType = { type: typeof SEND_MESSAGE };
 
-export const updateNewMessageBodyCreator = (body: string) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body}) as const;
-export const sendMessageCreator = () => ({type: SEND_MESSAGE}) as const;
+export const updateNewMessageBodyCreator = (body: string) => ({ type: UPDATE_NEW_MESSAGE_BODY, body: body }) as const;
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE }) as const;
--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,48 +1,48 @@
-import {ActionType} from "./redux-store";
+import { ActionType } from "./redux-store";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 export type PostType = {
-    id: number,
-    message: string,
-    likesCount: number
+  id: number,
+  message: string,
+  likesCount: number
 }
 
 export type ProfilePageStateType = {
-    posts: Array<PostType>,
-    newPostText: string
+  posts: Array<PostType>,
+  newPostText: string
 }
 
 const initialState: ProfilePageStateType = {
-    posts: [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It is my first post', likesCount: 11},
-        {id: 3, message: 'Blala', likesCount: 11},
-        {id: 4, message: 'Dada', likesCount: 11},
-    ],
-    newPostText: ''
+  posts: [
+    { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+    { id: 2, message: 'It is my first post', likesCount: 11 },
+    { id: 3, message: 'Blala', likesCount: 11 },
+    { id: 4, message: 'Dada', likesCount: 11 },
+  ],
+  newPostText: ''
 }
 
 export const profileReducer = (state: ProfilePageStateType = initialState, action: ActionType) => {
-    switch (action.type) {
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
-        case ADD_POST:
-            let message = state.newPostText;
-            let newPost = {id: 7, message: message, likesCount: 0};
-
-            state.newPostText = '';
-            state.posts.push(newPost);
-            return state;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case UPDATE_NEW_POST_TEXT:
+      return {
+        ...state,
+        newPostText: action.newText
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, { id: 7, message: state.newPostText, likesCount: 0 }]
+      }
+    default:
+      return state;
+  }
 }
 
 export type updateNewPostActionType = { type: typeof UPDATE_NEW_POST_TEXT, newText: string };
 export type addPostActionType = { type: typeof ADD_POST };
 
-export const updateNewPostTextActionCreator = (text: string) => ({type: UPDATE_NEW_POST_TEXT, newText: text}) as const;
-export const addPostActionCreator = () => ({type: ADD_POST}) as const;
+export const updateNewPostTextActionCreator = (text: string) => ({ type: UPDATE_NEW_POST_TEXT, newText: text }) as const;
+export const addPostActionCreator = () => ({ type: ADD_POST }) as const;
